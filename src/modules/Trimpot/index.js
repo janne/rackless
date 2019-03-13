a19de40484e31050128091974a91e82685b68827
Jan Andersson <jan.andersson@gmail.com>
2019-03-13 22:44:31 +0100

    Make use of mouse dragging for turning

--- src/modules/Trimpot/index.js
+++ src/modules/Trimpot/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 import * as R from "ramda"
 import { changeTrimpot } from "../../store/actions"
@@ -8,16 +8,40 @@ const styles = {
   content: { position: "absolute" }
 }
 
+const dragImg = new Image(0, 0)
+dragImg.src =
+  "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+dragImg.style.display = "none"
+
 const Trimpot = ({ x, y, width = 30, mod, pot, value = 0, changeTrimpot }) => {
-  const clickHandler = () => {
-    changeTrimpot(mod, pot, value + 1)
+  const [dragPos, setDragPos] = useState(null)
+
+  const dragStartHandler = e => {
+    setDragPos({ x: e.pageX, y: e.pageY, value })
+    e.dataTransfer.setDragImage(dragImg, 0, 0)
   }
 
+  const dragEndHandler = e => setDragPos(null)
+
+  const dragHandler = e => {
+    if (e.pageX) {
+      const newValue =
+        dragPos.value + (e.pageX - dragPos.x + (dragPos.y - e.pageY)) / 10
+      if (value !== newValue) changeTrimpot(mod, pot, newValue)
+    }
+  }
+
+  const dblClickHandler = e => changeTrimpot(mod, pot, 0)
+
   return (
-    <div style={{ ...styles.content, left: x, top: y }} onClick={clickHandler}>
+    <div style={{ ...styles.content, left: x, top: y }}>
       <img
+        onDrag={dragHandler}
+        onDragEnd={dragEndHandler}
+        onDragStart={dragStartHandler}
+        onDoubleClick={dblClickHandler}
         src={background}
-        style={{ width, transform: `rotate(${value * 10}deg)` }}
+        style={{ ...styles.img, width, transform: `rotate(${value * 10}deg)` }}
         alt="Trimpot"
       />
     </div>
