37a056e09462a02c7b2f987774d8ae2f0540a0c9
Jan Andersson <jan.andersson@gmail.com>
2019-03-31 21:32:03 +0200

    Break out common instrument code

--- src/components/Output/Instrument.js
+++ src/components/Output/Instrument.js
@@ -1,41 +1,9 @@
+import Module from "../Module"
 import Tone from "tone"
 
-const ranges = {
-  audio: Tone.Type.AudioRange,
-  normal: Tone.Type.NormalRange,
-  frequency: Tone.Type.Frequency
-}
-
-export default class extends Tone.Instrument {
-  defaults = {
-    gain: 1
-  }
-
-  tones = []
-
-  makeTone(Class, ...args) {
-    const tone = new Class(...args)
-    this.tones.push(tone)
-    return tone
-  }
-
-  constructor(pots = [], inputs = [], outputs = []) {
-    super()
-    this.createInsOuts(Object.keys(inputs).length, Object.keys(outputs).length)
-
-    pots.forEach(pot => {
-      this[pot.name] = this.makeTone(Tone.Signal, 0, ranges[pot.range])
-    })
-
-    inputs.forEach(i => {
-      this[i.name] = this.makeTone(Tone.Signal, 0, ranges[i.range])
-      this.input[i.socketId] = this[i.name]
-    })
-
-    outputs.forEach(o => {
-      this[o.name] = this.makeTone(Tone.Signal, 0, ranges[o.range])
-      this.output[o.socketId] = this[o.name]
-    })
+export default class extends Module {
+  constructor(pots, inputs) {
+    super(pots, inputs)
 
     this.masterGain = this.makeTone(Tone.Gain)
     this.gain.connect(this.masterGain.gain)
@@ -63,10 +31,5 @@ export default class extends Tone.Instrument {
     this.level3.connect(this.gain3r.gain)
     this.panner3r = this.makeTone(Tone.Panner, 1)
     this.i3r.chain(this.gain3r, this.panner3r, this.masterGain)
-  }
-
-  dispose() {
-    this.tones.forEach(t => t.dispose())
-    super.dispose()
   }
 }
--- src/components/VCO/Instrument.js
+++ src/components/VCO/Instrument.js
@@ -1,38 +1,10 @@
+import Module from "../Module"
 import Tone from "tone"
 import AudioToFrequency from "./AudioToFrequency"
 
-const ranges = {
-  audio: Tone.Type.AudioRange,
-  normal: Tone.Type.NormalRange,
-  frequency: Tone.Type.Frequency
-}
-
-export default class extends Tone.Instrument {
-  tones = []
-
-  makeTone(Class, ...args) {
-    const tone = new Class(...args)
-    this.tones.push(tone)
-    return tone
-  }
-
-  constructor(pots = [], inputs = [], outputs = []) {
-    super()
-    this.createInsOuts(Object.keys(inputs).length, Object.keys(outputs).length)
-
-    pots.forEach(pot => {
-      this[pot.name] = this.makeTone(Tone.Signal, 0, ranges[pot.range])
-    })
-
-    inputs.forEach(i => {
-      this[i.name] = this.makeTone(Tone.Signal, 0, ranges[i.range])
-      this.input[i.socketId] = this[i.name]
-    })
-
-    outputs.forEach(o => {
-      this[o.name] = this.makeTone(Tone.Signal, 0, ranges[o.range])
-      this.output[o.socketId] = this[o.name]
-    })
+export default class extends Module {
+  constructor(pots, inputs, outputs) {
+    super(pots, inputs, outputs)
 
     const { Sine, Triangle, Sawtooth, Square } = Tone.Oscillator.Type
     const types = [Sine, Triangle, Sawtooth, Square]
@@ -76,10 +48,5 @@ export default class extends Tone.Instrument {
 
       osc.start().connect(this.output[idx])
     })
-  }
-
-  dispose() {
-    this.tones.forEach(t => t.dispose())
-    super.dispose()
   }
 }
