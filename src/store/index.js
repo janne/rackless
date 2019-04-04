5f355fd81eca23867b0a28a14533b36996ebaf21
Jan Andersson <jan.andersson@gmail.com>
2019-04-04 08:18:54 +0200

    Remove redux dev tools for now

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "./reducers"
 
-export default createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+export default createStore(rootReducer, applyMiddleware(thunk))
