de256ecccfd3ef96bacf2ee25122984ac76cfacd
Jan Andersson <jan.andersson@gmail.com>
2019-04-06 21:17:15 +0200

    Cables only takes space needed

--- src/components/Cable/Bezier/index.js
+++ src/components/Cable/Bezier/index.js
@@ -1,27 +1,29 @@
 import React from "react"
-import {
-  HEIGHT_PIX,
-  HP_PIX,
-  ZOOM,
-  MAX_COLS,
-  MAX_ROWS,
-  CABLE_SLACK
-} from "../../../constants"
+import { ZOOM, CABLE_SLACK } from "../../../constants"
 
 const Cable = ({ x1, y1, x2, y2, color }) => {
   const hang = Math.cbrt(Math.abs(x2 - x1)) * CABLE_SLACK
+  const strokeWidth = 3 * ZOOM
+  const left = Math.min(x1, x2) - strokeWidth
+  const top = Math.min(y1, y2) - strokeWidth
+  const width = Math.abs(x2 - x1) + 2 * strokeWidth
+  const height = Math.abs(y2 - y1) + 2 * strokeWidth + hang
   return (
-    <div style={{ position: "absolute", pointerEvents: "none" }}>
-      <svg
-        width={MAX_COLS * HP_PIX}
-        height={MAX_ROWS * HEIGHT_PIX}
-        xmlns="http://www.w3.org/2000/svg"
-      >
+    <div
+      style={{
+        position: "absolute",
+        pointerEvents: "none",
+        left,
+        top
+      }}
+    >
+      <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
         <path
-          d={`M${x1} ${y1} C ${x1} ${y1 + hang}, ${x2} ${y2 +
-            hang}, ${x2} ${y2}`}
+          d={`M${x1 - left} ${y1 - top} C ${x1 - left} ${y1 -
+            top +
+            hang}, ${x2 - left} ${y2 - top + hang}, ${x2 - left} ${y2 - top}`}
           stroke={color}
-          strokeWidth={3 * ZOOM}
+          strokeWidth={strokeWidth}
           opacity={0.8}
           strokeLinecap="round"
           fill="transparent"
--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,6 @@
 export const CABLE_SLACK = 10
 export const HEIGHT_MM = 128.5
 export const HP_MM = 5.08
-export const MAX_COLS = 84
-export const MAX_ROWS = 5
 export const ZOOM = 3
 export const HEIGHT_PIX = HEIGHT_MM * ZOOM
 export const HP_PIX = HP_MM * ZOOM
