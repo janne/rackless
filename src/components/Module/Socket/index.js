6ad5ad47e0a01d7355b95691462086fd8b3ea57f
Jan Andersson <jan.andersson@gmail.com>
2019-04-07 00:15:40 +0200

    Support dragging from unused output socket

--- src/components/Module/Socket/index.js
+++ src/components/Module/Socket/index.js
@@ -1,18 +1,24 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
+import * as R from "ramda"
 import { connect } from "react-redux"
+import Draggable from "react-draggable"
 import background from "./background.svg"
 import { ZOOM } from "../../../constants"
-import { dispatchAndPersist, createCable } from "../../../store/actions"
+import {
+  dispatchAndPersist,
+  createCable,
+  dragConnector,
+  moveConnector
+} from "../../../store/actions"
 import { getDB } from "../../../store/selectors"
 
-const noop = () => {}
+const CENTER = 3.3 * ZOOM
 
 const styles = {
   content: { position: "absolute" },
   background: { width: 7 * ZOOM },
   draggable: { cursor: "pointer" }
 }
-
 const COLORS = [
   "red",
   "green",
@@ -35,25 +41,55 @@ const Socket = ({
   direction,
   dispatchAndPersist
 }) => {
-  const newCable = () => {
-    dispatchAndPersist(createCable(nextKey, moduleId, socketId, randomColor()))
-  }
+  const [newCable] = useState({
+    x: x * ZOOM - 4,
+    y: y * ZOOM - 4,
+    key: nextKey
+  })
+
   return (
-    <div
-      className="draggable"
-      style={{ ...styles.content, left: x * ZOOM, top: y * ZOOM }}
-    >
-      <img
-        draggable={false}
-        src={background}
-        style={{
-          ...styles.background,
-          ...(direction === "outputs" ? styles.draggable : {})
-        }}
-        alt="Socket"
-        onClick={direction === "outputs" ? newCable : noop}
-      />
-    </div>
+    <Fragment>
+      <div
+        className="draggable"
+        style={{ ...styles.content, left: x * ZOOM, top: y * ZOOM }}
+      >
+        <img
+          draggable={false}
+          src={background}
+          style={styles.background}
+          alt="Socket"
+        />
+      </div>
+      {direction === "outputs" && (
+        <Draggable
+          position={R.pick(["x", "y"], newCable)}
+          onStart={(e, data) => {
+            dispatchAndPersist(
+              createCable(newCable.key, moduleId, socketId, randomColor())
+            )
+          }}
+          onDrag={(e, data) => {
+            const pos = { x: e.x - CENTER, y: e.y - CENTER }
+            dispatchAndPersist(dragConnector(newCable.key, "inputs", pos))
+          }}
+          onStop={(e, data) => {
+            const pos = { x: e.x - CENTER, y: e.y - CENTER }
+            dispatchAndPersist(moveConnector(newCable.key, "inputs", pos))
+          }}
+        >
+          <div
+            className="draggable"
+            style={{
+              top: 0,
+              left: 0,
+              position: "absolute",
+              width: 27,
+              height: 27
+            }}
+          />
+        </Draggable>
+      )}
+    </Fragment>
   )
 }
 
