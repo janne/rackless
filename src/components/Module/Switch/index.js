9c5f134d8054c9a5de79e244a8d9a4c0a59744c8
Jan Andersson <jan.andersson@gmail.com>
2019-04-09 00:02:18 +0200

    Support switching between two values

--- src/components/Module/Switch/index.js
+++ src/components/Module/Switch/index.js
@@ -13,8 +13,9 @@ const Switch = ({ x, y, id, name, range = [], value, setValue }) => {
     }
   }
 
+  const currentValue = value || range[0]
+
   const clickHandler = () => {
-    const currentValue = value || range[0]
     const index = R.findIndex(R.equals(currentValue), range)
     if (index >= 0) {
       const newIndex = (index + 1) % range.length
@@ -25,7 +26,15 @@ const Switch = ({ x, y, id, name, range = [], value, setValue }) => {
 
   return (
     <div style={styles.content} onClick={clickHandler}>
-      <img draggable={false} src={background} alt="Trimpot" />
+      <img
+        draggable={false}
+        src={background}
+        style={{
+          ...styles.img,
+          transform: `rotate(${currentValue === range[0] ? 0 : 180}deg)`
+        }}
+        alt="Switch"
+      />
     </div>
   )
 }
