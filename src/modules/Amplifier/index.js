dd7515367b15150da272f77f548535a5cea2e214
Jan Andersson <jan.andersson@gmail.com>
2019-04-10 01:08:13 +0200

    Added controls to Amplifier

--- src/modules/Amplifier/index.js
+++ src/modules/Amplifier/index.js
@@ -1,3 +1,17 @@
 import background from "./background.svg"
 
-export default { background }
+const controls = [
+  { name: "level1", x: 47, y: 37, range: "normal" },
+  { name: "level2", x: 47, y: 180, range: "normal" }
+]
+
+const inputs = []
+
+const outputs = []
+
+const setup = ({ inputs, outputs, controls, background }) => {
+  const tones = []
+  return () => Object.values(tones).forEach(t => t.dispose())
+}
+
+export default { inputs, outputs, controls, setup, background }
