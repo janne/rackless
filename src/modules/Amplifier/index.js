e67d25952a5f96ce9de382cdb6615513a128d8e3
Jan Andersson <jan.andersson@gmail.com>
2019-04-10 20:53:56 +0200

    Wire up Amplifier module

--- src/modules/Amplifier/index.js
+++ src/modules/Amplifier/index.js
@@ -1,3 +1,4 @@
+import Tone from "tone"
 import background from "./background.svg"
 
 const controls = [
@@ -5,12 +6,40 @@ const controls = [
   { name: "level2", x: 47, y: 180, range: "normal" }
 ]
 
-const inputs = []
+const inputs = [
+  { name: "cv1", x: 10, y: 46, range: "normal" },
+  { name: "input1", x: 10, y: 102, range: "audio" },
+  { name: "cv2", x: 10, y: 189, range: "normal" },
+  { name: "input2", x: 10, y: 245, range: "audio" }
+]
 
-const outputs = []
+const outputs = [
+  { name: "output1", x: 56, y: 102, range: "audio" },
+  { name: "output2", x: 56, y: 245, range: "audio" },
+  { name: "mix", x: 56, y: 315, range: "audio" }
+]
 
 const setup = ({ inputs, outputs, controls, background }) => {
-  const tones = []
+  const tones = {}
+
+  tones.gain1 = new Tone.Gain()
+  tones.add1 = new Tone.Add()
+  controls.level1.connect(tones.add1, 0, 0)
+  inputs.cv1.connect(tones.add1, 0, 1)
+  tones.add1.connect(tones.gain1.gain)
+  inputs.input1.chain(tones.gain1)
+  tones.gain1.connect(outputs.output1)
+  tones.gain1.connect(outputs.mix)
+
+  tones.gain2 = new Tone.Gain()
+  tones.add2 = new Tone.Add()
+  controls.level2.connect(tones.add2, 0, 0)
+  inputs.cv2.connect(tones.add2, 0, 1)
+  tones.add2.connect(tones.gain2.gain)
+  inputs.input2.chain(tones.gain2)
+  tones.gain2.connect(outputs.output2)
+  tones.gain2.connect(outputs.mix)
+
   return () => Object.values(tones).forEach(t => t.dispose())
 }
 
