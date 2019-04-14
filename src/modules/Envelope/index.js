62c9fdd540e42b2329025353cce9260c07eab490
Jan Andersson <jan.andersson@gmail.com>
2019-04-14 09:16:05 +0200

    Hook up envelope

--- src/modules/Envelope/index.js
+++ src/modules/Envelope/index.js
@@ -1,6 +1,10 @@
+import Tone from "tone"
 import background from "./background.svg"
 
-const inputs = { gate: { x: 5, y: 272 }, retrig: { x: 36, y: 272 } }
+const inputs = {
+  gate: { x: 5, y: 272 },
+  retrig: { x: 36, y: 272 }
+}
 
 const outputs = {
   out: { range: "audio", x: 36, y: 323 },
@@ -8,12 +12,28 @@ const outputs = {
 }
 
 const controls = {
-  attack: { x: 12, y: 37 },
-  decay: { x: 12, y: 96 },
-  release: { x: 12, y: 154 },
-  sustain: { x: 12, y: 213 }
+  attack: { x: 12, y: 37, range: "time" },
+  decay: { x: 12, y: 96, range: "time" },
+  sustain: { x: 12, y: 213 },
+  release: { x: 12, y: 154, range: "time" }
 }
 
-const setup = (inputs, outputs, controls, background) => {}
+const setup = ({ inputs, outputs, controls }) => {
+  const tones = {
+    envelope: new Tone.Envelope(),
+    analyser: new Tone.Analyser("waveform", 32)
+  }
+
+  tones.envelope.attack = controls.attack
+  tones.envelope.decay = controls.decay
+  tones.envelope.sustain = controls.sustain
+  tones.envelope.release = controls.release
+
+  inputs.gate.connect(tones.analyser)
+
+  tones.envelope.connect(outputs.out)
+
+  return () => Object.values(tones).forEach(t => t.dispose())
+}
 
 export default { inputs, outputs, controls, setup, background }
