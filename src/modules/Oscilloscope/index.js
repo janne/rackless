b00eae6d1d14eb58996699f06288bcb988ea6c5a
Jan Andersson <jan.andersson@gmail.com>
2019-04-16 01:31:07 +0200

    Initial work on oscilloscope

--- src/modules/Oscilloscope/index.js
+++ src/modules/Oscilloscope/index.js
@@ -1,7 +1,12 @@
 import React from "react"
+import * as R from "ramda"
+import Tone from "tone"
 import background from "./background.svg"
 
-const Component = props => <div style={{ width: 144, height: 144 }} />
+const width = 144
+const height = 144
+
+const Component = (_, ref) => <canvas ref={ref} width={width} height={height} />
 
 const controls = {
   select: { x: 11, y: 201 },
@@ -24,11 +29,45 @@ const outputs = {
   d: { x: 117, y: 309 }
 }
 
-const setup = ({ inputs, outputs }) => {
-  inputs.a.connect(outputs.a)
-  inputs.b.connect(outputs.b)
-  inputs.c.connect(outputs.c)
-  inputs.d.connect(outputs.d)
+const setup = ({ inputs, outputs, controls }) => {
+  const tones = {
+    analyserA: new Tone.Analyser("waveform", 256),
+    analyserB: new Tone.Analyser("waveform", 256),
+    analyserC: new Tone.Analyser("waveform", 256),
+    analyserD: new Tone.Analyser("waveform", 256)
+  }
+
+  inputs.a.chain(tones.analyserA, outputs.a)
+  inputs.b.chain(tones.analyserB, outputs.b)
+  inputs.c.chain(tones.analyserC, outputs.c)
+  inputs.d.chain(tones.analyserD, outputs.d)
+
+  const dispose = () => Object.values(tones).forEach(t => t.dispose())
+
+  const scale = (value, istart, istop, ostart, ostop) =>
+    ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+
+  const loop = () => {
+    const canvas = controls.monitor.ref.current
+    const context = canvas.getContext("2d")
+    const value = tones.analyserA.getValue()
+    context.clearRect(0, 0, width, height)
+    context.beginPath()
+    const lineWidth = 2
+    context.lineWidth = lineWidth
+    let start = 0
+    value.slice(start, 128).forEach((v, i) => {
+      const x = scale(i, 0, 128, 0, width)
+      const y = scale(v, -1, 1, 0, height)
+      if (i === 0) context.moveTo(x, y)
+      else context.lineTo(x, y)
+    })
+    context.lineCap = "round"
+    context.strokeStyle = "white"
+    context.stroke()
+  }
+
+  return { dispose, loop }
 }
 
 export default { controls, inputs, outputs, setup, background }
