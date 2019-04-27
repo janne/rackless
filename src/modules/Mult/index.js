590bd8a8f957de06216a13b166f1afa3c9359c4c
Jan Andersson <jan.andersson@gmail.com>
2019-04-28 01:54:00 +0200

    Add Mult

---
+++ src/modules/Mult/index.js
@@ -0,0 +1,27 @@
+import Tone from "tone"
+import background from "./background.svg"
+
+export const inputs = {
+  in1: { x: 5, y: 40 },
+  in2: { x: 5, y: 208 }
+}
+
+export const outputs = {
+  out1a: { x: 5, y: 93 },
+  out1b: { x: 5, y: 129 },
+  out1c: { x: 5, y: 165 },
+  out2a: { x: 5, y: 261 },
+  out2b: { x: 5, y: 297 },
+  out2c: { x: 5, y: 333 }
+}
+
+const setup = ({ inputs, outputs }) => {
+  inputs.in1.connect(outputs.out1a)
+  inputs.in1.connect(outputs.out1b)
+  inputs.in1.connect(outputs.out1c)
+  inputs.in2.connect(outputs.out2a)
+  inputs.in2.connect(outputs.out2b)
+  inputs.in2.connect(outputs.out2c)
+}
+
+export default { background, inputs, outputs, setup }
