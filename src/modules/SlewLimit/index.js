a8966f7246a8089a860b542fd5cf41143c50918e
Jan Andersson <jan.andersson@gmail.com>
2019-04-28 11:15:06 +0200

    Add SlewLimit stub

---
+++ src/modules/SlewLimit/index.js
@@ -0,0 +1,38 @@
+import Tone from "tone"
+import background from "./background.svg"
+
+const controls = {
+  attack: { x: 12, y: 42 },
+  decay: { x: 12, y: 135 },
+  type: { x: 15, y: 245, range: ["exp", "lin"] }
+}
+
+const inputs = {
+  in: { x: 5, y: 312 },
+  attack: { x: 20, y: 83 },
+  decay: { x: 20, y: 176 }
+}
+
+const outputs = {
+  out: { x: 36, y: 313 }
+}
+
+const setup = ({ controls, inputs, outputs }) => {
+  const tones = {
+    analyser: new Tone.Analyser("waveform", 64),
+    signal: new Tone.Signal()
+  }
+
+  inputs.in.connect(tones.analyser)
+  tones.signal.connect(outputs.out)
+
+  const dispose = () => Object.values(tones).forEach(t => t.dispose())
+
+  const loop = () => {
+    tones.signal.value = tones.analyser.getValue()[0]
+  }
+
+  return { dispose, loop }
+}
+
+export default { background, controls, inputs, outputs, setup }
