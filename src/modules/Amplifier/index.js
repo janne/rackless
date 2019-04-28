f1c011561e97bad65c6d1586cf1b9cbe71b34246
Jan Andersson <jan.andersson@gmail.com>
2019-04-28 22:28:42 +0200

    Make separate level and bias controls on Amplifier

--- src/modules/Amplifier/index.js
+++ src/modules/Amplifier/index.js
@@ -2,42 +2,53 @@ import Tone from "tone"
 import background from "./background.svg"
 
 const controls = {
-  level1: { x: 47, y: 37 },
-  level2: { x: 47, y: 180 }
+  level1: { x: 5, y: 37 },
+  bias1: { x: 50, y: 37 },
+  level2: { x: 5, y: 187 },
+  bias2: { x: 50, y: 187 }
 }
 
 const inputs = {
-  cv1: { x: 10, y: 46 },
-  cv2: { x: 10, y: 189 },
-  input1: { x: 10, y: 102, range: "audio" },
-  input2: { x: 10, y: 245, range: "audio" }
+  cv1: { x: 35, y: 100 },
+  cv2: { x: 35, y: 250 },
+  in1: { x: 5, y: 100, range: "audio" },
+  in2: { x: 5, y: 250, range: "audio" }
 }
 
 const outputs = {
-  output1: { x: 56, y: 102, range: "audio" },
-  output2: { x: 56, y: 245, range: "audio" },
-  mix: { x: 56, y: 315, range: "audio" }
+  out1: { x: 66, y: 100, range: "audio" },
+  out2: { x: 66, y: 250, range: "audio" },
+  mix: { x: 66, y: 315, range: "audio" }
 }
 
 const setup = ({ inputs, outputs, controls }) => {
-  const tones = {}
+  const tones = {
+    gain1: new Tone.Gain(),
+    add1: new Tone.Add(),
+    level1: new Tone.Gain(),
+    double1: new Tone.Multiply(2),
+    gain2: new Tone.Gain(),
+    add2: new Tone.Add(),
+    level2: new Tone.Gain(),
+    double2: new Tone.Multiply(2)
+  }
 
-  tones.gain1 = new Tone.Gain()
-  tones.add1 = new Tone.Add()
-  controls.level1.connect(tones.add1, 0, 0)
-  inputs.cv1.connect(tones.add1, 0, 1)
+  controls.bias1.connect(tones.add1, 0, 0)
+  inputs.cv1.connect(tones.level1)
+  controls.level1.chain(tones.double1, tones.level1.gain)
+  tones.level1.connect(tones.add1, 0, 1)
   tones.add1.connect(tones.gain1.gain)
-  inputs.input1.chain(tones.gain1)
-  tones.gain1.connect(outputs.output1)
+  inputs.in1.chain(tones.gain1)
+  tones.gain1.connect(outputs.out1)
   tones.gain1.connect(outputs.mix)
 
-  tones.gain2 = new Tone.Gain()
-  tones.add2 = new Tone.Add()
-  controls.level2.connect(tones.add2, 0, 0)
-  inputs.cv2.connect(tones.add2, 0, 1)
+  controls.bias2.connect(tones.add2, 0, 0)
+  inputs.cv2.connect(tones.level2)
+  controls.level2.chain(tones.double2, tones.level2.gain)
+  tones.level2.connect(tones.add2, 0, 1)
   tones.add2.connect(tones.gain2.gain)
-  inputs.input2.chain(tones.gain2)
-  tones.gain2.connect(outputs.output2)
+  inputs.in2.chain(tones.gain2)
+  tones.gain2.connect(outputs.out2)
   tones.gain2.connect(outputs.mix)
 
   return { dispose: () => Object.values(tones).forEach(t => t.dispose()) }
